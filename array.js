c64bb1b4d3b91f35b6c5ec4726ee037a7ff8580b
Mani Chandana <mgudivad@gitam.in>
2026-01-08 22:38:47 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,17 @@
+// let myDetails = ["Mani Chandana", 20, "Biotechnology"];
+
+// console.log("Name:", myDetails[0]);
+// console.log("Age:", myDetails[1]);
+// console.log("Degree:", myDetails[2]);
+
+let myDetails = Array(); 
+
+myDetails[0] = "Mani Chandana";
+myDetails[1] = 20;
+myDetails[2] = "Biotechnology";
+
+// Displaying array values
+for (let i = 0; i < myDetails.length; i++) {
+  console.log(myDetails[i]);
+}
+
